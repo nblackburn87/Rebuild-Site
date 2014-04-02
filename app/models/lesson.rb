4223a5cb45198f11bd414830e0b8e0e58123380c
Natalie Blackburn & Kathryn Hough <student@epicodus.com>
2014-04-02 16:47:06 -0700

    Add section model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,7 +2,13 @@ class Lesson < ActiveRecord::Base
 
   validates :name, :presence => true
 
-  def self.renumber
+  # before_save do |lesson|
+  #   if lesson.sequence_id.exist?
+  #     self.renumber
+  #   end
 
-  end
+  # def self.renumber
+
+  # end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LearnToProgram::Application.routes.draw do
+  match('/', {via: :get, to: 'lessons#index'})
   match('lessons', {via: :get, to: 'lessons#index'})
   match('lessons', {via: :post, to: 'lessons#create'})
   match('lessons/new', {via: :get, to: 'lessons#new'})
@@ -6,4 +7,7 @@ LearnToProgram::Application.routes.draw do
   match('lessons/:id/edit', {via: :get, to: 'lessons#edit'})
   match('lessons/:id', {via: [:patch, :put], to: 'lessons#update'})
   match('lessons/:id', {via: :delete, to: 'lessons#destroy'})
+
+  match('sections', {via: :post, to: 'sections#create'})
+  match('sections/new', {via: :get, to: 'sections#new'})
 end
